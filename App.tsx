3955dd9fe49f06eaa7ea000dd53d969e3960c84f
killer-cf <costa.kilder@live.com>
2023-05-29 11:04:51 -0300

    project assets

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import { Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View>
+    <View style={{backgroundColor: '#202024', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+      />
       {fontsLoaded ? <Text>Hello world</Text> : <View />} 
     </View>
   );
