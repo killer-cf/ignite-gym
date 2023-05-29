40df6852937c134a0ad80571d831dee6a6e00066
killer-cf <costa.kilder@live.com>
2023-05-29 11:35:50 -0300

    config theme

--- App.tsx
+++ App.tsx
@@ -2,16 +2,19 @@ import { StatusBar, View } from 'react-native';
 import { NativeBaseProvider } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { theme } from './src/theme';
+import { Loading } from '@components/Loading';
+
 export default function App() {
   const [ fontsLoaded ] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={theme}>
       <StatusBar 
         barStyle='light-content'
         backgroundColor='transparent'
       />
-      {fontsLoaded ? <View /> : <View />} 
+      {fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   );
 }
