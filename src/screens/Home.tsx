a1bb4269b4d33a8f498ef840536ee0cfbb1e2f0e
killer-cf <costa.kilder@live.com>
2023-06-26 16:43:25 -0300

    style component Group

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,33 @@
-import { HomeHeader } from "@components/HomeHeader";
-import { VStack } from "native-base";
+import { useState } from 'react'
+import { VStack, FlatList } from "native-base"
+
+import { Group } from "@components/Group"
+import { HomeHeader } from "@components/HomeHeader"
 
 export function Home() {
+  const [groups, setGroups] = useState(['costas', 'bíceps', 'triceps', 'ombro'])
+  const [groupSelected, setGroupSelected] = useState('costas')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item}) => (
+          <Group 
+            name={item} 
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   )
 }
