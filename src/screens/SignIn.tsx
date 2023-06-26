7ed9b9ffc00677d046023f689e790c0896c68ad9
killer-cf <costa.kilder@live.com>
2023-06-26 10:56:24 -0300

    auth navigation handle and default image

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,7 @@
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base"
+import { useNavigation } from "@react-navigation/native"
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes"
+
 
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
@@ -6,11 +9,19 @@ import { Input } from "@components/Input"
 import { Button } from "@components/Button"
 
 export function SignIn() {
+
+  const navigation = useNavigation<AuthNavigatorRoutesProps>()
+
+  function handleNewAccount() {
+    navigation.navigate('signUp')
+  }
+
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1}} showsVerticalScrollIndicator={false}>
       <VStack flex={1} px={10} >
         <Image 
-          source={BackgroundImg} 
+          source={BackgroundImg}
+          defaultSource={BackgroundImg} 
           alt='Pessoas treinando'
           resizeMode="contain"
           position='absolute'  
@@ -48,6 +59,7 @@ export function SignIn() {
           </Text>
 
           <Button 
+            onPress={handleNewAccount}
             title="Criar conta" 
             variant={'outline'}
           />
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,4 +1,5 @@
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base"
+import { useNavigation } from "@react-navigation/native"
 
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
@@ -6,11 +7,18 @@ import { Input } from "@components/Input"
 import { Button } from "@components/Button"
 
 export function SignUp() {
+  const navigation = useNavigation()
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
+
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1}} showsVerticalScrollIndicator={false}>
       <VStack flex={1} px={10} >
         <Image 
           source={BackgroundImg} 
+          defaultSource={BackgroundImg} 
           alt='Pessoas treinando'
           resizeMode="contain"
           position='absolute'  
@@ -46,6 +54,7 @@ export function SignUp() {
         </Center>
 
         <Button 
+          onPress={handleGoBack}
           mt={24}
           title="Voltar para o login" 
           variant={'outline'}
