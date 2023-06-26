f12e703b5942f00f40a39632a3e06f3753fe29d0
killer-cf <costa.kilder@live.com>
2023-06-26 17:16:52 -0300

    style component ExerciseCard

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
-import { VStack, FlatList } from "native-base"
+import { VStack, FlatList, HStack, Heading, Text } from "native-base"
 
 import { Group } from "@components/Group"
 import { HomeHeader } from "@components/HomeHeader"
+import { ExerciseCard } from '@components/ExerciseCard'
 
 export function Home() {
   const [groups, setGroups] = useState(['costas', 'bíceps', 'triceps', 'ombro'])
@@ -28,6 +29,22 @@ export function Home() {
         my={10}
         maxH={10}
       />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent={'space-between'} mb={5}>
+          <Heading color={'gray.200'} fontSize={'md'}>
+            Exercícios
+          </Heading>
+
+          <Text color={'gray.200'} fontSize={'sm'}>
+            4
+          </Text>
+        </HStack>
+
+        <ExerciseCard />
+        <ExerciseCard />
+      </VStack>
+      
     </VStack>
   )
 }
