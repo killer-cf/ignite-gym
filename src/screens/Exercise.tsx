1c08af3a9821b4dfca6c813470ad1ea7215088bb
killer-cf <costa.kilder@live.com>
2023-06-27 11:33:28 -0300

    style exercise header screen

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,44 @@
-import { Center, Text } from "native-base";
+import { HStack, Heading, Icon, Text, VStack } from "native-base"
+import { TouchableOpacity } from "react-native"
+import { Feather } from "@expo/vector-icons"
+import { useNavigation } from "@react-navigation/native"
+import { AppNavigatorRoutesProps } from "@routes/app.routes"
+
+import BodySvg from '@assets/body.svg'
 
 export function Exercise() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function goBack() {
+    navigation.goBack()
+  }
+
+
   return (
-    <Center flex={1}>
-      <Text>Exercise</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px={8} bg={"gray.600"} pt={16}>
+        <TouchableOpacity onPress={goBack}>
+          <Icon 
+            as={Feather}
+            name="arrow-left"
+            color={"green.500"}
+            size={6}
+          />
+        </TouchableOpacity>
+
+        <HStack justifyContent={"space-between"} mt={4} mb={8} alignItems={"center"}>
+          <Heading fontFamily={"heading"} color={"gray.100"} fontSize={"lg"} flexShrink={1}>
+            Puxada frontal
+          </Heading>
+
+          <HStack alignItems={"center"}>
+            <BodySvg />
+            <Text color={"gray.200"} ml={1} textTransform={"capitalize"}>
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
+      </VStack>
+    </VStack>
   )
 }
