58d2d992be9a569d74c4594c89e731beddae37de
killer-cf <costa.kilder@live.com>
2023-06-27 10:07:55 -0300

    history list empty

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,6 +1,6 @@
 import { HistoryCard } from "@components/HistoryCard"
 import { ScreenHeader } from "@components/ScreenHeader"
-import { Heading, SectionList, VStack } from "native-base"
+import { Heading, SectionList, Text, VStack } from "native-base"
 import { useState } from "react"
 
 export function History() {
@@ -32,6 +32,13 @@ export function History() {
           </Heading>
         )}
         px={8}
+        contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: "center"}}
+        ListEmptyComponent={() => (
+          <Text color={"gray.100"} textAlign={"center"}>
+            Não há exercícios registrados ainda. {"\n"}
+            Vamos fazer exercícios hoje?
+          </Text>
+        )}
       />
     </VStack>
   )
