945690b24a274df0abeb4a5d2fe88baa44cc5083
killer-cf <costa.kilder@live.com>
2023-06-28 16:15:06 -0300

    persist user on asyncStorage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,7 +1,8 @@
 import { api } from "@services/api"
-import { createContext, ReactNode, useState } from "react"
+import { createContext, ReactNode, useEffect, useState } from "react"
 
 import { UserDTO } from "@dtos/UserDTO"
+import { storageUserGet, storageUserSave } from "@storage/storageUser"
 
 type AuthContextProviderProps = {
   children: ReactNode
@@ -10,12 +11,14 @@ type AuthContextProviderProps = {
 export type AuthContextDataProps = {
   user: UserDTO;
   signIn: (email: string, password: string) => Promise<void>
+  isLoadingUserStorageData: boolean
 }
 
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps)  {
   const [user, setUser] = useState<UserDTO>({} as UserDTO)
+  const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true)
 
   async function signIn(email: string, password: string) {
     try {
@@ -23,6 +26,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps)  {
 
       if (data.user) {
         setUser(data.user)
+        storageUserSave(data.user)
       }
     } catch (error) {
       throw error
@@ -30,8 +34,31 @@ export function AuthContextProvider({ children }: AuthContextProviderProps)  {
     
   }
 
+  async function loadUserData() {
+    try {
+      const userLogged = await storageUserGet()
+
+      if (userLogged) {
+        setUser(userLogged) 
+      }
+    } catch (error) {
+      throw error
+    } finally {
+      setIsLoadingUserStorageData(false)
+    }
+    
+  }
+
+  useEffect(() => {
+    loadUserData()
+  }, [])
+
   return (
-    <AuthContext.Provider value={{ user, signIn }}>
+    <AuthContext.Provider value={{ 
+      user, 
+      signIn,
+      isLoadingUserStorageData, 
+    }}>
       {children}
     </AuthContext.Provider>
   )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,22 @@
 import { useTheme, Box } from 'native-base'
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from './app.routes';
 import { useAuth } from '@hooks/useAuth';
+import { Loading } from '@components/Loading';
 
 export function Routes(){
   const { colors } = useTheme()
-  const { user } = useAuth()
+  const { user, isLoadingUserStorageData } = useAuth()
+
   console.log('usuario logado => ', user)
   const theme = DefaultTheme
   theme.colors.background = colors.gray[700]
+
+  if(isLoadingUserStorageData) {
+    return <Loading />
+  }
 
   return (
     <Box flex={1} bg={'gray.700'}>
