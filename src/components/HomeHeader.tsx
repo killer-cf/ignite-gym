86c5bb8ba333c2575c85b0965567620f4d8a28b9
killer-cf <costa.kilder@live.com>
2023-06-28 10:25:11 -0300

    fix Heading font weight bold not work

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -18,7 +18,7 @@ export function HomeHeader() {
           Olá, 
         </Text>
 
-        <Heading color={"gray.100"}  fontSize={"md"}>
+        <Heading color={"gray.100"} fontFamily={"heading"} fontSize={"md"}>
           Kilder
         </Heading>
       </VStack>
